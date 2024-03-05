9b53a39a85553fb4ba1f399ea47db12a25975d47
LuongLeVan <levanluong6a8@gmail.com>
2024-03-05 10:46:46 +0700

    add reponsive excercise

---
+++ homeworks/Nguyễn ngọc Quang_C4EJS148_responsive/main.js
@@ -0,0 +1,9 @@
+function showMobileMenu() {
+    var x = document.getElementById("mobileMenu");
+    x.style.display = "block";
+}
+
+function closeMobileMenu() {
+    var x = document.getElementById("mobileMenu");
+    x.style.display = "none";
+}
