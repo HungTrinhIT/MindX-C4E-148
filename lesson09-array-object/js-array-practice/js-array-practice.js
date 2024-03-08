06458b68952db3afc2195594ad63aae3fe40dc82
HungTrinhIT <minhhung.it.work@gmail.com>
2024-03-08 21:34:49 +0700

    add homeword

---
+++ lesson09-array-object/js-array-practice/js-array-practice.js
@@ -0,0 +1,25 @@
+/*
+    Bài 1: Tính tổng các số chẵn từ 1 tới n
+    Bài 2: Tính giá trị của n! (n>=0)
+           Giải thừa: 
+                    2! = 1 * 2
+                    3! =  1 * 2 * 3
+                    ....
+                    n = 1 * 2 * 3 * ... * n
+            Chú ý: 0! = 1! = 1
+
+    Bài 3: Từ mảng ban đầu hãy liệt kê 
+    kiểu dữ liệu của các giá trị trong mảng ban đầu
+
+    input: ["214", true, false, 2, 2.15, [], null]
+    expected output: ["string", "boolean", "boolean", "number", "number", "object", "object"]
+
+    Bài 4: tìm giá trị nhỏ nhất trong mảng 1 chiều
+
+    Bài 5: tìm giá trị lớn nhất trong mảng 1 chiều
+
+    Bài 6: Sắp xếp mảng theo thứ tự tăng dần
+
+    Bài 7 Sắp xếp mảng theo thứ tự giảm dần
+
+*/
